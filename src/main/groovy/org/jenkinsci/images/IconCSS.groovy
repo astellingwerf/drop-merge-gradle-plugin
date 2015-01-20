cfece86b93605df52a6e029d769889dffef7ebd7
Anne Stellingwerf <astellin@opentext.com>
2015-01-20 10:58:17 +0100

    Fix the way icons are loaded into IconCSS.

--- src/main/groovy/org/jenkinsci/images/IconCSS.groovy
+++ src/main/groovy/org/jenkinsci/images/IconCSS.groovy
@@ -9,29 +9,23 @@ class IconCSS {
         StringBuilder sb = new StringBuilder('.jenkinsJobStatus{background-repeat: no-repeat; padding-left: 19px; width: 16px; height: 16px;}').append System.lineSeparator()
 
         def extraStyles = [grey: ['aborted', 'disabled'], grey_anime: ['aborted_anime', 'disabled_anime']]
-        Enumeration<URL> resources = IconCSS.class.getClassLoader().getResources(IconCSS.class.package.name.replace('.', '/'))
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            new File(resource.getFile()).listFiles().each { File f ->
-                ['png', 'gif'].each { String extension ->
-                    if (f.name.endsWith(".$extension")) {
-                        sb.append '.jenkinsJobStatus_'
-                        sb.append f.name[0..-5]
-                        sb.append ' '
-                        if (extraStyles.containsKey(f.name[0..-5])) {
-                            extraStyles[f.name[0..-5]].each { String extraStyle ->
-                                sb.append ', .jenkinsJobStatus_'
-                                sb.append extraStyle
-                                sb.append ' '
-                            }
-                        }
-                        sb.append '{background-image: url(data:image/' + extension + ';base64,'
-                        sb.append f.bytes.encodeBase64().toString()
-                        sb.append ')}'
-                        sb.append System.lineSeparator()
-                    }
-                }
-            }
+		def images = ['blue','yellow','red','grey'].collectMany { ["${it}.png", "${it}_anime.gif"] }
+		images.each { String imageFileName ->
+        	sb.append '.jenkinsJobStatus_'
+			sb.append imageFileName[0..-5]
+			sb.append ' '
+			if (extraStyles.containsKey(imageFileName[0..-5])) {
+				extraStyles[imageFileName[0..-5]].each { String extraStyle ->
+					sb.append ', .jenkinsJobStatus_'
+					sb.append extraStyle
+					sb.append ' '
+				}
+			}
+			sb.append '{background-image: url(data:image/' + imageFileName[-3..-1] + ';base64,'
+			InputStream resource = IconCSS.class.getClassLoader().getResourceAsStream(IconCSS.class.package.name.replace('.', '/')+'/'+imageFileName)
+			sb.append resource.bytes.encodeBase64().toString()
+			sb.append ')}'
+			sb.append System.lineSeparator()
         }
 
         return sb.toString()
